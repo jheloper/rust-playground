fbf82595affee79384feb1dc34222926957ecb2c
Joonghyeon Kim <jheloper9102@gmail.com>
2019-04-14 15:41:17 +0900

    변수 섀도잉 코드 추가

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 fn main() {
 
+    // mutable variable
     let mut x = 5;
     println!("The value of x is: {}", x);
 
     x = 6;
     println!("The value of x is: {}", x);
 
+    // const
     const MAX_POINTS: u32 = 100_000;
     println!("MAX_POINTS is: {}", MAX_POINTS);
+
+    // shadowing
+    let y = 5;
+    let y = y + 1;
+    let y = y * 2;
+    println!("The value of y is: {}", y);
 }
