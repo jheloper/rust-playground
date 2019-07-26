0ac19cb12b90a8127dae9f60cc9f5e16f52f444e
Joonghyeon Kim <jheloper9102@gmail.com>
2019-07-26 23:34:29 +0900

    String 타입을 사용하는 예제 코드 추가

--- src/main.rs
+++ src/main.rs
@@ -108,6 +108,13 @@ fn main() {
     for index in (0..4).rev() {
         println!("for loop index is: {}", index);
     }
+
+    // Rust의 문자열 리터럴 값은 불변.
+    // 또한 컴파일 시점에서 값이 정해지지 않는 경우도 존재함.
+    // 따라서 문자열 리터럴이 아닌 문자열 타입인 String을 제공함.
+    let mut example_string = String::from("hello");
+    example_string.push_str(", world!");
+    println!("example String value is: {}", example_string);
 }
 
 fn another_function(x: i32, y: i32) {
