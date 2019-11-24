0af8beb481fca9ba474df00d2cc7234b69b16654
Joonghyeon Kim <jheloper9102@gmail.com>
2019-11-24 21:04:17 +0900

    필드 초기화 축약법 및 구조체 갱신 문법에 대한 예제 코드 추가

--- src/struct_example.rs
+++ src/struct_example.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct User {
     username: String,
     email: String,
@@ -7,23 +8,39 @@ struct User {
 
 pub fn example_struct() {
     let user1 = User {
-        email: String::from("someone@example.com"),
+        email: String::from("user1@example.com"),
         username: String::from("username123"),
         active: true,
         sign_in_count: 1,
     };
+    println!("user1 is {:?}", user1);
 
-    println!("user1 email is {}, username is {}", user1.email, user1.username);
+    let user2 = build_user(String::from("user2@example.com"), String::from("username123"));
+    println!("user2 is {:?}", user2);
 
-    let user2 = build_user(String::from("someone@example.com"), String::from("username123"));
+    // user2의 일부 값을 재사용하여 새로운 User 인스턴스 생성.
+    let user3 = User {
+        email: String::from("user3@example.com"),
+        username: String::from("username456"),
+        active: user2.active,
+        sign_in_count: user2.sign_in_count
+    };
+    println!("user3 is {:?}", user3);
 
-    println!("user2 email is {}, username is {}", user2.email, user2.username);
+    // 구조체 갱신 문법(struct update syntax)으로 user2의 일부 값을 재사용.
+    let user4 = User {
+        email: String::from("user4@example.com"),
+        username: String::from("username789"),
+        ..user2
+    };
+    println!("user4 is {:?}", user4);
 }
 
 fn build_user(email: String, username: String) -> User {
+    // 변수명과 구조체의 필드명이 같다면 아래처럼 필드 초기화 축약법(field init shorthand)을 이용할 수 있음.
     User {
-        email: email,
-        username: username,
+        email,
+        username,
         active: true,
         sign_in_count: 1
     }
