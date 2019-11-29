f400085818656fc4946366a2ac58c0e6bc969692
Joonghyeon Kim <jheloper9102@gmail.com>
2019-11-29 23:56:48 +0900

    사각형 넓이를 구하는 예제 코드에 구조체를 이용하도록 리팩터링

--- src/struct_rectangles_example.rs
+++ src/struct_rectangles_example.rs
@@ -13,6 +13,13 @@ pub fn example_struct_rectangles() {
         "The area of the rectangle is {} square pixels.",
         calculate_area_by_tuple(rect1)
     );
+
+    // 위 코드를 구조체를 이용하는 코드로 리팩터링.
+    let rect2 = Rectangle { length: 50, width: 30 };
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        calculate_area_by_struct(&rect2)
+    );
 }
 
 fn calculate_area(length: u32, width: u32) -> u32 {
@@ -22,4 +29,13 @@ fn calculate_area(length: u32, width: u32) -> u32 {
 fn calculate_area_by_tuple(dimensions: (u32, u32)) -> u32 {
     // 튜플 인덱스가 무엇을 의미하는지 기억해야 한다...
     dimensions.0 * dimensions.1
+}
+
+struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+fn calculate_area_by_struct(rectangle: &Rectangle) -> u32 {
+    rectangle.length * rectangle.width
 }
