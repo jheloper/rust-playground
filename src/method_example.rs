3537f63ed1b9f22ba6a9891ca0fac41751ff88e3
Joonghyeon Kim <jheloper9102@gmail.com>
2019-12-02 19:18:23 +0900

    self가 아닌 다른 파라미터를 가진 메서드 예제 코드 추가

--- src/method_example.rs
+++ src/method_example.rs
@@ -7,13 +7,21 @@ impl Rectangle {
     fn calculate_area(&self) -> u32 {
         self.length * self.width
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
 }
 
 pub fn example_method() {
     let rect1 = Rectangle { length: 50, width: 30 };
+    let rect2 = Rectangle { length: 40, width: 20 };
+    let rect3 = Rectangle { length: 60, width: 20 };
 
     println!(
         "The area of the rectangle is {} square pixels.",
         rect1.calculate_area()
     );
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
