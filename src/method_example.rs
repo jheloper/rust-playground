0cb9bc85d238f77cff3887ce6da6ec0a14d38913
Joonghyeon Kim <jheloper9102@gmail.com>
2019-12-03 23:12:44 +0900

    정사각형을 생성하는 연관 함수 예제 코드 추가

--- src/method_example.rs
+++ src/method_example.rs
@@ -11,12 +11,28 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.length > other.length && self.width > other.width
     }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            length: size,
+            width: size,
+        }
+    }
 }
 
 pub fn example_method() {
-    let rect1 = Rectangle { length: 50, width: 30 };
-    let rect2 = Rectangle { length: 40, width: 20 };
-    let rect3 = Rectangle { length: 60, width: 20 };
+    let rect1 = Rectangle {
+        length: 50,
+        width: 30,
+    };
+    let rect2 = Rectangle {
+        length: 40,
+        width: 20,
+    };
+    let rect3 = Rectangle {
+        length: 60,
+        width: 20,
+    };
 
     println!(
         "The area of the rectangle is {} square pixels.",
@@ -24,4 +40,10 @@ pub fn example_method() {
     );
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let square = Rectangle::square(30);
+    println!(
+        "The area of the square is {} square pixels.",
+        square.calculate_area()
+    );
 }
