7f11488b89a2bdb37ada3c1fcc0f10f550323cfc
Joonghyeon Kim <jheloper9102@gmail.com>
2019-12-17 22:10:20 +0900

    Option 열거형의 Some, None에 대한 예제 코드 추가

--- src/enums_example.rs
+++ src/enums_example.rs
@@ -7,11 +7,11 @@ pub fn example_enums() {
 
     let home1 = IpAddr {
         kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1")
+        address: String::from("127.0.0.1"),
     };
     let loopback1 = IpAddr {
         kind: IpAddrKind::V6,
-        address: String::from("::1")
+        address: String::from("::1"),
     };
 
     println!("home ip address is {:?}", home1);
@@ -24,7 +24,7 @@ pub fn example_enums() {
     println!("loopback ip address is {:?}", loopback2);
 
     let message1 = Message::Quit;
-    let message2 = Message::Move {x: 7, y: 12};
+    let message2 = Message::Move { x: 7, y: 12 };
     let message3 = Message::Write(String::from("hello"));
     let message4 = Message::ChangeColor(15, 10, 30);
 
@@ -32,6 +32,24 @@ pub fn example_enums() {
     message2.call();
     message3.call();
     message4.call();
+
+    let some_number = Some(5);
+    let some_string = Some("a string");
+    let absent_number: Option<i32> = None;
+
+    println!("some number is {:?}", some_number);
+    println!("some string is {:?}", some_string);
+    println!("absent number is {:?}", absent_number);
+
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+
+    // 아래 코드는 i8과 Option<i8>을 더하려고 하기 때문에 컴파일 오류 발생.
+    // let sum = x + y;
+    
+    // Option<T>의 unwrap 메서드를 사용하여 i8 값을 추출.
+    let sum = x + y.unwrap();
+    println!("x + y is {}", sum);
 }
 
 #[derive(Debug)]
