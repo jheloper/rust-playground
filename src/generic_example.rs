6d224384a9606c09a5e9a1607d52146a8c372737
Joonghyeon Kim <jheloper9102@gmail.com>
2020-01-10 23:52:53 +0900

    같은 동작을 수행하나 이름과 시그니처가 다른 두 함수 예제 코드 추가

--- src/generic_example.rs
+++ src/generic_example.rs
@@ -1,16 +1,47 @@
 pub fn example_generic() {
-    let numbers = vec![34, 50, 25, 100, 65];
+    let numbers1 = vec![34, 50, 25, 100, 65];
 
-    let result = largest(&numbers);
-    println!("The largest number is {}", result);
+    let result1 = largest(&numbers1);
+    println!("The largest number is {}", result1);
 
-    let numbers = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let numbers2 = vec![102, 34, 6000, 89, 54, 2, 43, 8];
 
-    let result = largest(&numbers);
-    println!("The largest number is {}", result);
+    let result2 = largest(&numbers2);
+    println!("The largest number is {}", result2);
+
+    let result3 = largest_i32(&numbers2);
+    println!("The largest number is {}", result3);
+
+    let chars = vec!['y', 'm', 'a', 'q'];
+    let result4 = largest_char(&chars);
+    println!("The largest char is {}", result4);
 }
 
 fn largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest_i32(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest_char(list: &[char]) -> char {
     let mut largest = list[0];
 
     for &item in list.iter() {
