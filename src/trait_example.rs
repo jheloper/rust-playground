0383d5f296c143940fd335d5821985249e818c46
Joonghyeon Kim <jheloper9102@gmail.com>
2020-01-15 23:50:50 +0900

    트레잇의 기본 구현 메서드를 사용하는 예제 코드 추가

--- src/trait_example.rs
+++ src/trait_example.rs
@@ -7,10 +7,28 @@ pub fn example_trait() {
     };
 
     println!("news article summary is {}", news_article.summary());
+
+    let tweet = Tweet {
+        username: String::from("Peter"),
+        content: String::from("this is tweet content!"),
+        reply: false,
+        retweet: false
+    };
+
+    println!("tweet summary is {}", tweet.summary());
+
+    let feed = Feed {
+        username: String::from("John"),
+        content: String::from("this is feed content!")
+    };
+
+    println!("feed summary is {}", feed.summary());
 }
 
 trait Summarizable {
-    fn summary(&self) -> String;
+    fn summary(&self) -> String {
+        String::from("(Read more...)")
+    }
 }
 
 struct NewsArticle {
@@ -25,3 +43,23 @@ impl Summarizable for NewsArticle {
         format!("{}, by {} ({})", self.headline, self.author, self.location)
     }
 }
+
+struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summarizable for Tweet {
+    fn summary(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+struct Feed {
+    pub username: String,
+    pub content: String
+}
+
+impl Summarizable for Feed {}
